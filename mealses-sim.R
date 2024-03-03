e1b8e990e5049c79e6019ff37991c2609909bc38
Jennifer McNichol <jmcnicho@unb.ca>
2024-03-03 10:18:56 -0800

    update sims

--- mealses-sim.R
+++ mealses-sim.R
@@ -39,7 +39,16 @@ measles.sim <- function(vax.rate,pop.size,intervention=F){
                  k=1/6, # mean E duration of 6 days before infectiousness
                  gamma=1/8) # 8 day infectiousness wo the qi  ) # parameters
   }else if (intervention==T){
-    # need to update 
+    # for now, all interventions on 
+    params <- list(beta = b,
+                   c=0.2, # the Es are a little infectious -- pre-symptom 
+                   v=0.05, # if on: 0.05 ( 0.01-0.1) rate of vaccination of S . makes a diff! 
+                   qs = 0, # does not make much diff if on: 0.06 (qs = 0.014 - 0.125 ) rate we find and quarantine susceptible people 
+                   qspep = 0.04, # if on: 2/3 qs quarantine and/or PEP for exposed people
+                   qi=0.2, # if on: 0.45 ( 0.2-0.72)  quarantine for infectious people (send home/isolate)
+                   l=1/15, # mean duration of quarantine is 21 days but people do it imperfectly but some are infectious, gah! 
+                   k=1/6, # mean E duration of 6 days before infectiousness
+                   gamma=1/8) # 8 day infectiousness wo the qi  ) # parameters
   }
   
   
@@ -62,5 +71,29 @@ noint.1000.80 <- measles.sim(0.80,1000,F)
 noint.1000.75 <- measles.sim(0.75,1000,F)
 noint.1000.70 <- measles.sim(0.70,1000,F)
 noint.1000.65 <- measles.sim(0.65,1000,F)
+
+int.1000.95 <- measles.sim(0.95,1000,T)
+int.1000.90 <- measles.sim(0.90,1000,T)
+int.1000.85 <- measles.sim(0.85,1000,T)
+int.1000.80 <- measles.sim(0.80,1000,T)
+int.1000.75 <- measles.sim(0.75,1000,T)
+int.1000.70 <- measles.sim(0.70,1000,T)
+int.1000.65 <- measles.sim(0.65,1000,T)
+
+noint.8000.95 <- measles.sim(0.95,1000,F)
+noint.8000.90 <- measles.sim(0.90,1000,F)
+noint.8000.85 <- measles.sim(0.85,1000,F)
+noint.8000.80 <- measles.sim(0.80,1000,F)
+noint.8000.75 <- measles.sim(0.75,1000,F)
+noint.8000.70 <- measles.sim(0.70,1000,F)
+noint.8000.65 <- measles.sim(0.65,1000,F)
+
+int.8000.95 <- measles.sim(0.95,1000,T)
+int.8000.90 <- measles.sim(0.90,1000,T)
+int.8000.85 <- measles.sim(0.85,1000,T)
+int.8000.80 <- measles.sim(0.80,1000,T)
+int.8000.75 <- measles.sim(0.75,1000,T)
+int.8000.70 <- measles.sim(0.70,1000,T)
+int.8000.65 <- measles.sim(0.65,1000,T)
 
 #save the results 
